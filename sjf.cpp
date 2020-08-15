7d6cfecc47a81226175e8f16358bc149674a3cdf
mhhazimm09 <55779779+mhhazimm09@users.noreply.github.com>
2020-08-16 00:55:50 +0800

    Update sjf.cpp
    Changed "Processes" into "Classes" in the output

--- sjf.cpp
+++ sjf.cpp
@@ -57,7 +57,7 @@ void findAverageTime(Process proc[], int n)
     findTurnAroundTime(proc, n, wt, tat);
  
     // display processes along with all details
-    cout << "\nProcesses "<< " Burst time "
+    cout << "\nClasses   "<< " Burst time "
          << " Waiting time " << " Turn around time\n";
  
     // calculate total waiting time and total turn around time
